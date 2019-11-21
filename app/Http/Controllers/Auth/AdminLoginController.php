7143332d51f80c246f3ed43543317799cda94e4c
ADNAN TURJOY <aturjoy4@gmail.com>
2019-11-21 13:51:20 +0600

    multiauth

---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+
+class AdminLoginController extends Controller
+{
+	public function __construct(){
+		$this->middleware('guest:admin');
+	}
+    public function showLoginForm(){
+    	return view('auth.admin-login');
+    }
+   //validate form data
+    public function login(Request $request){
+    	$this->validate($request,[
+           'email' => 'required|email',
+           'password' => 'required|min:6'
+    	]);
+
+    	//attempt to log the user in
+    	if (Auth::guard('admin')->attempt(['email'=> $request->email,'password'=>$request->password],$request->remember)) {
+    		return redirect()->intended(route('admin.dashboard'));
+    	}
+
+    	return redirect()->back()->withInput($request->only('email','remember')); 
+
+    }
+}
